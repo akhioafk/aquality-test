80696b3bfa47514c8dd524eac6f2b0fcd5d220a9
Akzhol Aidossuly <aidosulyakzhol@gmail.com>
2025-10-16 16:09:11 +0500

    vertex

--- aquality-selenium-template-cucumber/src/test/java/aquality/selenium/template/cucumber/hooks/AqualityServicesHooks.java
+++ aquality-selenium-template-cucumber/src/test/java/aquality/selenium/template/cucumber/hooks/AqualityServicesHooks.java
@@ -2,6 +2,7 @@ package aquality.selenium.template.cucumber.hooks;
 
 import aquality.selenium.browser.AqualityServices;
 import aquality.selenium.template.modules.CustomBrowserModule;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class AqualityServicesHooks {
@@ -9,5 +10,10 @@ public class AqualityServicesHooks {
     @Before(order = 0)
     public void reinitializeAqualityServices() {
         AqualityServices.initInjector(new CustomBrowserModule());
+    }
+
+    @After(order = 0)
+    public void quitAqualityServices() {
+        AqualityServices.quit();
     }
 }
